36a4e7d56f7f6ddec4bb6775a5d4e9f1617f445a
Raghu2491 <raghavendra0204@gmail.com>
2017-04-12 15:43:57 -0500

    Initial Commit

---
+++ Vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vector
+//
+//  Created by Admin Gowda on 4/12/17.
+//  Copyright © 2017 Admin Gowda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
